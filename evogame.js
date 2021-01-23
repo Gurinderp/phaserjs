03fa98ba649b27b95d0abaa4f8efe356b0c0d77c
rednirug <sgurinderpreet@gmail.com>
2021-01-23 04:37:25 -0800

    removed squittle physics and added W(up) and S(down) keys

--- evogame.js
+++ evogame.js
@@ -8,19 +8,20 @@ class EvoGame extends Phaser.Scene {
 	}
 
 	create() {
-		let player = this.physics.add.image(400, 300, "Squittle");
-		player.setBounce(0.2);
-		player.setCollideWorldBounds(true);
+		let player = this.add.image(400, 300, "Squittle");
 		console.log(this.player);
 
 		this.input.keyboard.on("keydown-D", function (event) {
-			player.x += 10;
+			player.x += 20;
 		});
 		this.input.keyboard.on("keydown-A", function (event) {
-			player.x -= 10;
+			player.x -= 20;
 		});
-		this.input.keyboard.on("keydown-SPACEBAR", function (event) {
-			player.y += 30;
+		this.input.keyboard.on("keydown-W", function (event) {
+			player.y -= 20;
+		});
+		this.input.keyboard.on("keydown-S", function (event) {
+			player.y += 20;
 		});
 	}
 
